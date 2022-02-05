f4e0063552ab8886e4a5bd0d58437db5a7ac0d3c
Shawn Anderson <shawnltf@pm.me>
2022-02-04 22:50:32 -0800

    Distributions work.

---
+++ parameters/.ipynb_checkpoints/fixtures-checkpoint.py
@@ -0,0 +1,45 @@
+distributions = [
+    {
+        'founders': 0.1,
+        'dao_rewards': 0.5,
+        'lbp_pool': 0.4,
+    }
+]
+
+
+roles = [
+    {
+        'name': 'CEO',
+        'base_wage': 2000,
+    },
+    {
+        'name': 'CFO',
+        'base_wage': 2000,
+    },
+    {
+        'name': 'CTO',
+        'base_wage': 2000,
+    },
+    {
+        'name': 'CMO',
+        'base_wage': 2000,
+    },
+    {
+        'name': 'Token Engineer 1',
+        'base_wage': 2000,
+    },
+    {
+        'name': 'Token Engineer 2',
+        'base_wage': 4000,
+    },
+]
+
+founders = [
+    {
+        'first_name': 'Shawn',
+        'last_name': 'Anderson',
+        'NDA': True,
+        'onboarded': True,
+        'active': True,
+    }
+]
